14915045149b70afd06a6a83fcb261acf9d61e90
Niveditha133 <69324426+Niveditha133@users.noreply.github.com>
2022-04-26 17:34:23 -0700

    Initial commit

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class InputHandler : MonoBehaviour
+{
+    public Vector2 InputVector { get; private set; }
+
+    public Vector3 MousePosition { get; private set; }
+
+    public void SetInputVector(CallbackContext ctx)
+    {
+        InputVector = ctx.ReadValue<Vector2>();
+    }
+}
